a4530677cb8a7c5b841b275cf0e686f3dff705c7
Cristian021000 <N00148478@upn.pe>
2023-08-05 01:11:04 -0500

    v0.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { RouterProvider} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Routers from './router/routesConfig'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Routers}/>
   </React.StrictMode>,
 )
